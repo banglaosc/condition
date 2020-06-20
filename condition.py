5066821ec411f715f970e67f970c0a0d087dc371
Sushen Biswas <sushenbiswasaga@gmail.com>
2020-06-20 15:03:38 +0600

    String

--- condition.py
+++ condition.py
@@ -3,40 +3,44 @@
 # এদের মধ্যে কে আজকের তারিখে বেশি ধনি
 # তা আমরা ইফ এলস স্টেমেন্টর মাধ্যমে দেখবো ।
 
+# https://www.businessinsider.in/thelife/personalities/news/top-100-richest-people-in-the-world/articleshow/74415418.cms
+
 # বিল_গেটস = int(input("বিল_গেটস এর টাকা পরিমান : "))
 # জুকারবার্গ = int(input("জুকারবার্গ এর টাকা পরিমান : "))
 # ওয়ারেন_বাফেট = int(input("ওয়ারেন_বাফেট এর টাকা পরিমান : "))
 # ইলন_মাস্ক = int(input("ইলন_মাস্ক এর টাকা পরিমান : "))
 
-from script import get_richest_person_worth
-বিল_গেটস = int(get_richest_person_worth('Bill Gates'))
+# from script import get_richest_person_worth
+# বিল_গেটস = int(get_richest_person_worth('Bill Gates'))
 # জুকারবার্গ = int(get_richest_person_worth('Bill Gates'))
 # ওয়ারেন_বাফেট = int(get_richest_person_worth('Bill Gates'))
 # ইলন_মাস্ক = int(get_richest_person_worth('Bill Gates'))
 
 # print(বিল_গেটস)
 #
-# বিল_গেটস = 25
-# জুকারবার্গ = 30
+
+বিল_গেটস = 25
+জুকারবার্গ = 30
+
 # ওয়ারেন_বাফেট = 35
 # ইলন_মাস্ক = 10
 
 
 
-# if বিল_গেটস > জুকারবার্গ:
-#     print("বিল_গেটস বেশি ধনি")
-# else:
-#     print("জুকারবার্গ বেশি ধনি")
+if বিল_গেটস > জুকারবার্গ:
+    print("বিল গেটস বেশি ধনি")
+else:
+    print("জুকারবার্গ বেশি ধনি")
     
 
-if ওয়ারেন_বাফেট > বিল_গেটস:
-    print("ওয়ারেন_বাফেট বেশি ধনি")
-elif বিল_গেটস > জুকারবার্গ:
-    print("বিল_গেটস বেশি ধনি")  
-elif জুকারবার্গ > ইলন_মাস্ক:
-    print("জুকারবার্গ বেশি ধনি")
-else:
-    print("ইলন_মাস্ক বেশি ধনি")
+# if ওয়ারেন_বাফেট > বিল_গেটস:
+#     print("ওয়ারেন বাফেট বেশি ধনি")
+# elif বিল_গেটস > জুকারবার্গ:
+#     print("বিল গেটস বেশি ধনি")
+# elif জুকারবার্গ > ইলন_মাস্ক:
+#     print("জুকারবার্গ বেশি ধনি")
+# else:
+#     print("ইলন মাস্ক বেশি ধনি")
 
 
 
